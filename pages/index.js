4465b5eb6cd7ea067d005ce888669cfc3dede395
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2022-07-04 12:39:02 +0545

    ggg

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">sandip olee's blog</a>
+          Welcome to sandip olee's blog
         </h1>
 
         <p className={styles.description}>
